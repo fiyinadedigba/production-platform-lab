1cf00c9b8664b30e0c0135ccef0dd551a99f1da9
fiyinadedigba <fionashmu@gmail.com>
2026-04-25 00:47:53 +0200

    Add latency metrics

--- app/src/server.js
+++ app/src/server.js
@@ -11,10 +11,30 @@ const httpRequestCounter = new client.Counter({
   help: "Total number of HTTP requests",
   labelNames: ["method", "route", "status_code"],
 });
+const httpRequestDuration = new client.Histogram({
+  name: "http_request_duration_seconds",
+  help: "HTTP request duration in seconds",
+  labelNames: ["method", "route", "status_code"],
+  buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2, 5],
+});
 
 app.use((req, res, next) => {
   res.on("finish", () => {
     httpRequestCounter.inc({
+      method: req.method,
+      route: req.path,
+      status_code: res.statusCode,
+    });
+  });
+
+  next();
+});
+
+app.use((req, res, next) => {
+  const end = httpRequestDuration.startTimer();
+
+  res.on("finish", () => {
+    end({
       method: req.method,
       route: req.path,
       status_code: res.statusCode,
